74721139cc55f34ffc9310bb56e35e4d7ec8082f
taesu <taesu@crscube.co.kr>
2019-04-22 10:10:22 +0900

    #Locale
    Application Locale에서 Locale alias를 통해 여러 Locale 헤더에 대한
    일괄처리 가능하도록 변경

--- src/main/java/ojt/crscube/locale/domain/model/ApplicationLocale.java
+++ src/main/java/ojt/crscube/locale/domain/model/ApplicationLocale.java
@@ -5,8 +5,12 @@ import lombok.Getter;
 import ojt.crscube.i18n.domain.exception.UnSupportedLocaleException;
 import org.springframework.context.i18n.LocaleContextHolder;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.function.Function;
+
+import static java.util.stream.Stream.of;
 
 /**
  * Created by taesu at : 2019-04-12
@@ -19,30 +23,33 @@ import java.util.function.Function;
  */
 @AllArgsConstructor @Getter
 public enum ApplicationLocale {
-    EN(Locale.ENGLISH, "English", 0),
-    KO(Locale.KOREAN, "한국어", 1);
+    EN(Locale.ENGLISH, Arrays.asList(Locale.ENGLISH), "English", 0),
+    KO(Locale.KOREAN, Arrays.asList(Locale.KOREA, Locale.KOREAN), "한국어", 1);
 
     private Locale locale;
+    private List<Locale> alias;
     private String name;
     private Integer order;
+
+    public boolean containsAlias(Locale target) {
+        return this.alias.contains(target);
+    }
 
     public static ApplicationLocale getDefaultLocale() {
         return ApplicationLocale.EN;
     }
 
-    public static final Function<Locale, ApplicationLocale> localeToApplicationLocale = requestLocale -> {
-        try {
-            return ApplicationLocale.valueOf(requestLocale.toString().toUpperCase());
-        } catch (Exception e) {
-            throw new UnSupportedLocaleException(requestLocale.toString());
-        }
-    };
-
     public static ApplicationLocale getCurrentLocale() {
         try {
-            return localeToApplicationLocale.apply(LocaleContextHolder.getLocale());
+            return CONVERTER.apply(LocaleContextHolder.getLocale());
         } catch (UnSupportedLocaleException e) {
             return getDefaultLocale();
         }
     }
+
+    public static final Function<Locale, ApplicationLocale> CONVERTER
+            = requestLocale -> of(ApplicationLocale.values())
+                                .filter(appLocale -> appLocale.containsAlias(requestLocale))
+                                .findAny()
+                                .orElseThrow(() -> new UnSupportedLocaleException(requestLocale.toString()));
 }
